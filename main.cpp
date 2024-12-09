d847aa25e61263c70d3cba2d0d594cfdc7ab7541
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-09 14:47:52 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main() {
 
+    int a;
+    int b;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter another number: ";
+    cin >> b;
+    cout << "a = " << a << " and b = " << b << endl;
+
+    int *pa = &b;
+    int *pb = &a;
+
+    cout << "a = " << *pa << " and b = " << *pb << endl;
 
     return 0;
 }
