36a5a3244f2117b927746ea7495f46e2a17cb7d2
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-09 14:39:50 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main() {
 
+    int a;
+    cout << "Enter a number: ";
+    cin >> a;
+
+    if (*(&a) > 0) {
+        cout << " Number is positive";
+    }
+    else if (*(&a) < 0) {
+        cout << "Number is negative";
+    }
+    else {
+        cout << "Number is zero";
+    }
 
     return 0;
 }
