3aa6e1ac9631d4279f52c4d30aa44ed511c68fce
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-09 15:04:16 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main() {
 
+    int size = 10;
+    int *arr = new int[size];
 
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 10;
+        cout << arr[i] << " ";
+        count += arr[i];
+    }
+    cout << endl;
+    cout << "Sum of the array: " << count << endl;
+
+    delete [] arr;
     return 0;
 }
