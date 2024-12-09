8ba523318a1b270e99f81188e237f2e5b10636a3
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-09 15:38:20 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -3,6 +3,29 @@ using namespace std;
 
 int main() {
 
+    int size = 10;
+    int arr_1[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr_2[size];
+
+    int *p_1 = arr_1;
+    int *p_2 = arr_2;
+
+    cout << "Array 1: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr_1[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < size; i++) {
+        *p_2 = *p_1;
+        p_1++;
+        p_2++;
+    }
+
+    cout << "Array 2: ";
+    for (int i = size - 1; i >= 0; i--) {
+        cout << arr_2[i] << " ";
+    }
 
     return 0;
 }
