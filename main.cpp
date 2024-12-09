17d309accb5411bfaa5b4af64ae9af6f8b65f318
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-09 14:54:57 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -3,6 +3,35 @@ using namespace std;
 
 int main() {
 
+    int a;
+    int b;
+    char c;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter a number: ";
+    cin >> b;
+    cout << "Enter an operator: ";
+    cin >> c;
+
+    int *pa = &a;
+    int *pb = &b;
+    char *pc = &c;
+
+    if (*pc == '+') {
+        cout << *pa << "+" << *pb << "=" << *pa + *pb << endl;
+    }
+    else if (*pc == '-') {
+        cout << *pa << "-" << *pb << "=" << *pa - *pb << endl;
+    }
+    else if (*pc == '*') {
+        cout << *pa << "*" << *pb << "=" << *pa * *pb << endl;
+    }
+    else if (*pc == '/') {
+        cout << *pa << "/" << *pb << "=" << *pa / *pb << endl;
+    }
+    else {
+        cout << "Invalid operator" << endl;
+    }
 
     return 0;
 }
