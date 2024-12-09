5f7bc8b91344789784ac132e06509e3a174ba7a3
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-09 14:34:44 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 int main() {
 
+    int a;
+    int b;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    if (a > b) {
+        int *pb = &b;
+        cout << a << " > " << *pb << endl;
+    }
+    else if (a < b) {
+        int *pa = &a;
+        cout << b << " > " << *pa << endl;
+    }
+    else {
+        cout << a << " = " << b << endl;
+    }
 
     return 0;
 }
