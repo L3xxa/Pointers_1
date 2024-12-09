aebbfb6ba5c30422b4dc92f64e059e5f9c9daf14
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-09 15:32:11 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main() {
 
+    int size = 10;
+    int arr_1[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int *p_1 = arr_1;
+
+    cout << "-------------> Before <-------------" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr_1[i] << " ";
+    }
+    cout << endl;
+
+    cout << "-------------> After <-------------" << endl;
+    for (int i = size - 1; i >= 0; i--) {
+        cout << arr_1[i] << " ";
+    }
 
     return 0;
 }
